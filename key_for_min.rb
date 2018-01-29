7019da64dbab554150ca9da68ccdd3b4e7d01bf4
Eden Mesfun <emesfun@gmail.com>
2018-01-29 14:51:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_key = ""
   lowest_key = Float::INFINITY
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     if value is the smallest
       return key
 end
